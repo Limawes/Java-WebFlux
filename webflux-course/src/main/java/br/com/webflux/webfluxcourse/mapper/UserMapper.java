f1ed7412cdbf8dd45a978be14eaf5bb5d9627db0
Limawes <wesleyaz0909@gmail.com>
2023-05-11 15:05:50 -0300

    mapper do user implementado e adicionado ao metodo save

--- webflux-course/src/main/java/br/com/webflux/webfluxcourse/mapper/UserMapper.java
+++ webflux-course/src/main/java/br/com/webflux/webfluxcourse/mapper/UserMapper.java
@@ -5,8 +5,12 @@ import br.com.webflux.webfluxcourse.model.request.UserRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 @Mapper(
-        componentModel = "spring"
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = IGNORE,
+        nullValueCheckStrategy = ALWAYS
 )
 public interface UserMapper {
 
